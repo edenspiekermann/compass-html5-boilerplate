46ec99fb1eae83c94dc093342491b2ce4d6e4f44
Alex Coles <alex@alexcolesportfolio.com>
2010-10-04 12:37:39 +0200

    Quick fixes to support use of Helpers in a Padrino app
    * Padrino's tag helper method has a different method signature to Rails'.
    
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- lib/app/helpers/html5_boilerplate_helper.rb
+++ lib/app/helpers/html5_boilerplate_helper.rb
@@ -3,10 +3,10 @@ module Html5BoilerplateHelper
   # http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither
   def ie_tag(name=:body, attrs={}, &block)
     attrs.symbolize_keys!
-    haml_concat("<!--[if lt IE 7 ]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->".html_safe)
-    haml_concat("<!--[if IE 7 ]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->".html_safe)
-    haml_concat("<!--[if IE 8 ]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->".html_safe)
-    haml_concat("<!--[if IE 9 ]>    #{ tag(name, add_class('ie9', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if lt IE 7 ]> #{ tag(name, add_class('ie6', attrs)) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 7 ]>    #{ tag(name, add_class('ie7', attrs)) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 8 ]>    #{ tag(name, add_class('ie8', attrs)) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 9 ]>    #{ tag(name, add_class('ie9', attrs)) } <![endif]-->".html_safe)
     haml_concat("<!--[if (gt IE 9)|!(IE)]><!-->".html_safe)
     haml_tag name, attrs do
       haml_concat("<!--<![endif]-->".html_safe)
@@ -37,7 +37,7 @@ private
     classes.strip!
     classes = ' ' + classes if !classes.blank?
     classes = name + classes
-    attrs.merge(:class => classes)
+    attrs.merge(:class => classes, :open => true)
   end
 
   def google_config(key)
